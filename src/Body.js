54ef01eb0e74779d22fcaa2f7840edfa21b9ce19
vivek666 <amalhan43@gmail.com>
2020-09-14 01:59:47 +0530

    Body banner implemented

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Body.css";
+import Header from "./Header";
+import { useDataLayerValue } from "./DataLayer";
 
 function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useDataLayerValue();
   return (
     <div className="body">
       <Header spotify={spotify} />
+      <div className="body__info">
+        <img src={discover_weekly.image[0].url} alt="" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
     </div>
   );
 }
