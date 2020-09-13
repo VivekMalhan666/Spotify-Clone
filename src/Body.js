c7671922b8c4a4d775eddf4742bda9c7b930b1d4
vivek666 <amalhan43@gmail.com>
2020-09-13 23:21:19 +0530

    Body header implemented

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Body.css";
 
-function Body() {
+function Body({ spotify }) {
   return (
     <div className="body">
-      <h1>I am the body</h1>
+      <Header spotify={spotify} />
     </div>
   );
 }
