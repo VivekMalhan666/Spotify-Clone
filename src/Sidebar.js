f6d39d9e30122a88b637ba9c660b10c696031c63
vivek666 <amalhan43@gmail.com>
2020-09-13 22:01:04 +0530

    Playlists rendered for sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,8 +4,11 @@ import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 import SidebarOption from "./SidebarOption";
+import { useDataLayerValue } from "./DataLayer";
 
 function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
+
   return (
     <div className="sidebar">
       <img
@@ -16,8 +19,13 @@ function Sidebar() {
       <SidebarOption Icon={HomeIcon} title="Home" />
       <SidebarOption Icon={SearchIcon} title="Search" />
       <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
-      <hr />
+      <br />
       <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+
+      {playlists?.items?.map((playlists) => (
+        <SidebarOption title={playlists.name} />
+      ))}
     </div>
   );
 }
